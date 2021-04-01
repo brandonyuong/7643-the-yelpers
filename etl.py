4c7c2f2600e781243b061075786e89b7168cb319
PortfolioSQA <59346324+PortfolioSQA@users.noreply.github.com>
2021-03-31 17:41:18 -0700

    etl file

--- etl.py
+++ etl.py
@@ -7,27 +7,30 @@ Created on Wed Mar 31 14:22:43 2021
 """
 import json
 import pandas as pd
-# data_file = open("./data/yelp_academic_dataset_review.json")
-# data = []
-# for line in data_file:
-#     data.append(json.loads(line))
-# review_df = pd.DataFrame(data)
-# data_file.close()
+
+# download dataset from https://www.kaggle.com/yelp-dataset/yelp-dataset
+
+#load reviews from downloaded json file
+data_file = open("./data/yelp_academic_dataset_review.json")
+data = []
+for line in data_file:
+    data.append(json.loads(line))
+review_df = pd.DataFrame(data)
+data_file.close()
+
+# load business from downloaded json file
+data_file = open("./data/yelp_academic_dataset_business.json")
+data = []
+for line in data_file:
+    data.append(json.loads(line))
+business_df = pd.DataFrame(data)
+data_file.close()
 
 # review_df.to_csv("./data/reviews.csv", index=False)
-
-# data_file = open("./data/yelp_academic_dataset_business.json")
-# data = []
-# for line in data_file:
-#     data.append(json.loads(line))
-# business_df = pd.DataFrame(data)
-# data_file.close()
-
 # business_df.to_csv("./data/business.csv", index=False)
 
-review_df = pd.read_csv("./data/reviews.csv")
-business_df = pd.read_csv("./data/business.csv")
-
+# review_df = pd.read_csv("./data/reviews.csv")
+# business_df = pd.read_csv("./data/business.csv")
 
 review_df = review_df[['business_id', 'stars', 'text']]
 business_df = business_df[['business_id', 'name', 'city', 'state', 'stars', 'review_count', 'is_open', 'categories']]
@@ -47,6 +50,7 @@ print(type(result.stars[0]))
 result = result[result['stars'].notna()]
 # replace stars with 1 or 2 with 'neg'
 # replace stars with 4 or 5 with 'pos'
+
 result.loc[result['stars'] == 1, 'stars'] = 'neg'
 result.loc[result['stars'] == 2, 'stars'] = 'neg'
 result.loc[result['stars'] == 4, 'stars'] = 'pos'
@@ -57,5 +61,14 @@ print(result.loc[66])
 result['text'] = result['text'].astype(str)
 final_df = result.groupby(['business_id','stars'])['text'].apply(lambda x: ','.join(x)).reset_index()
 
-print(final_df.loc[1])
-print(final_df.text[1])
+# print(len(final_df)) #143009
+
+print(final_df.business_id.loc[1])
+print(final_df.stars.loc[0])
+print(final_df.text.loc[0])
+print()
+print()
+print()
+print(final_df.business_id.loc[1])
+print(final_df.stars.loc[1])
+print(final_df.text.loc[1])
